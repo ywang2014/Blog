2d2d77992b465afb9910fe67a4f8b3735d8b8376
wangyong.1991 <wangyong.1991@bytedance.com>
2019-07-23 18:20:37 +0800

    add heapSortTopK

--- tech/algorithm/topk/HeapSortTopK.java
+++ tech/algorithm/topk/HeapSortTopK.java
@@ -11,10 +11,69 @@ import java.util.List;
 public class HeapSortTopK extends BaseTopK {
     @Override
     public void findTopK(int[] arr, int low, int hi, int k, List<Integer> result) {
+        if (arr == null || low < 0 || hi > arr.length || low >= hi || k > hi - low) {
+            throw new IllegalArgumentException("Arguments error.");
+        }
 
+        int[] heap = new int[k];
+        for (int i = 0; i < k; i++) {
+            heap[i] = arr[start+i];
+        }
+        buildHead(heap);
+
+        for (int i = start+k; i < end; i++) {
+            if (arr[i] > heap[0]) {
+                heap[0] = arr[i];
+                downloadFilter(heap, 0);
+            }
+        }
+
+        for (int i = 0; i < k; i++) {
+            result.add(heap[i]);
+        }
+    }
+
+    private void buildHead(int[] arr) {
+        for (int i = arr.length/2; i >= 0; i--) {
+            downloadFilter(arr, i);
+        }
+    }
+
+    private void downloadFilter(int[] arr, int index) {
+        int lChild = 2*index + 1;
+        int rChild = 2*index + 2;
+        if (lChild >= arr.length) {
+            return;
+        }
+
+        if (rChild >= arr.length) {
+            if (lChild < arr.length && arr[lChild] < arr[index]) {
+                swap(arr, lChild, index);
+                downloadFilter(arr, lChild);
+            }
+        }
+
+        int child = lChild;
+        if (arr[lChild] > arr[rChild]) {
+            // 取更小的一个
+            child = rChild;
+        }
+
+        if (child < arr.length && arr[child] < arr[index]) {
+            swap(arr, child, index);
+            downloadFilter(arr, child);
+        }
+    }
+
+    private void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 
     public static void main(String[] args) {
-
+        HeapSortTopK topK = new HeapSortTopK();
+        int[] arr = {1, 2, 3, 4, 5, 8, 5, 4, 6};
+        topK.findTopK(arr, 0, arr.length, 3);
     }
 }
